61ff628d4883814449b9f9b5d197fde5a10ce404
Gideon <gowusumsse@gmail.com>
2020-07-14 17:59:19 -0700

    Finished set up of preliminary listview.

--- app/src/main/java/com/example/pokedex/CustomListView.kt
+++ app/src/main/java/com/example/pokedex/CustomListView.kt
@@ -1,2 +1,72 @@
 package com.example.pokedex
 
+import android.widget.ArrayAdapter
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+class CustomListView : ArrayAdapter<String>{
+    private var name : List<String> = listOf()
+    private var desc : List<String> = listOf()
+    private var image : List<Int> = listOf()
+    private var context: Activity
+
+    constructor(context: Activity, name: List<String>, desc: List<String>, image: List<Int>) : super(context,R.layout.activity_main,name){
+        this.context = context
+            this.name = name
+            this.desc = desc
+            this.image = image
+    }
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var r : View? = convertView
+        val vwHolder: ViewHolder?
+
+        if (r==null)
+        {
+            val layoutInflater : LayoutInflater = context.layoutInflater
+            r = layoutInflater.inflate(R.layout.list_layout, null,true)
+            vwHolder = ViewHolder(r)
+            r.tag = vwHolder
+        }
+        else{
+            vwHolder = r.tag as ViewHolder?
+
+        }
+
+        if (vwHolder != null) {
+            vwHolder.imageVw?.setImageResource(image[position])
+        }
+
+        if (vwHolder != null) {
+            vwHolder.textVw1?.text = name[position]
+        }
+
+        if (vwHolder != null) {
+            vwHolder.textVw2?.text = desc[position]
+        }
+
+
+
+
+        return r as View
+    }
+
+    class ViewHolder{
+
+        var textVw1: TextView? = null
+        var textVw2: TextView? = null
+        var imageVw: ImageView? = null
+
+        constructor(view: View)  {
+            textVw1 = view.findViewById(R.id.tvPokemonType)
+            textVw2 = view.findViewById(R.id.tvPokemonDesc)
+            imageVw = view.findViewById(R.id.imageView)
+        }
+    }
+
+}
--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,27 +1,41 @@
 package com.example.pokedex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private val name = listOf("Die 1", "Die 2", "Die 3", "Die 4", "Die 5", "Die 6")
+    private val desc = listOf("1","2", "3", "4", "5", "6")
+    private val image = listOf(R.drawable.dice_1,R.drawable.dice_2,R.drawable.dice_3,R.drawable.dice_4,R.drawable.dice_5,R.drawable.dice_6)
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         CoroutineScope(Dispatchers.Main).launch{ //All functionalities must be executed in this scope as it is the main thread
-            val tv = findViewById<TextView>(R.id.textView)
+            ///val tv = findViewById<TextView>(R.id.textView)
 
-            tv.text = downloaderTask("https://pokeapi.co/api/v2/pokemon/ditto")
+         // tv.text = downloaderTask("https://pokeapi.co/api/v2/pokemon/ditto")
+
+//            //My test
+//            val pokemon = listOf(downloaderTask("https://pokeapi.co/api/v2/pokemon/"))
+//           val pokemonDesc = getJSONInfo("name", pokemon.toString())
+
 
         }
 
+        listView =  findViewById(R.id.listview)
 
-
+        val customListView = CustomListView(this,name, desc, image)
+        listView.adapter = customListView
     }
 }
